6eaacb6fbba13bde2ed363197837005adcdcfa86
Amir Vahedi <amirvahedix@gmail.com>
2024-04-16 18:24:52 +0330

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Amir on 1/28/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
