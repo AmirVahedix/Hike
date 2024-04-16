fd5d4b190e26186b4a6b0fc67e12e5c99d798065
Amir Vahedi <amirvahedix@gmail.com>
2024-04-16 20:08:42 +0330

    Implemented Header in CardView

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -10,22 +10,59 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
-            Circle()
-              .fill(
-                LinearGradient(
-                  colors: [
-                    Color("ColorIndigoMedium"),
-                    Color("ColorSalmonLight")
-                 ],
-                 startPoint: .topLeading,
-                 endPoint: .bottomTrailing
-                )
-              )
-              .frame(width: 256, height: 256)
-            Image("image-1")
-                .resizable()
-                .scaledToFit()
-        }
+            CustomBackgroundView()
+            VStack {
+                // MARK: - HEADER
+                VStack (alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [
+                                        .customGrayLight,
+                                        .customGrayMedium
+                                    ],
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                )
+                        )
+                        Spacer()
+                        Button {
+                            print("This shit works.")
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal, 30)
+                
+                // MARK: - MAIN CONTENT
+                ZStack {
+                    Circle()
+                      .fill(
+                        LinearGradient(
+                          colors: [
+                            Color("ColorIndigoMedium"),
+                            Color("ColorSalmonLight")
+                         ],
+                         startPoint: .topLeading,
+                         endPoint: .bottomTrailing
+                        )
+                      )
+                      .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+        } //: Card
+        .frame(width: 320, height: 570)
     }
 }
 
---
+++ Hike/Component/CustomButtonView.swift
@@ -0,0 +1,52 @@
+//
+//  CustomButtonView.swift
+//  Hike
+//
+//  Created by Amir on 1/28/1403 AP.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .white,
+                            .customGreenLight,
+                            .customGreenMedium
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+            Circle()
+                .stroke(
+                    LinearGradient(
+                        colors: [.customGrayLight, .customGrayMedium],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+            Image(systemName: "figure.hiking")
+                .fontWeight(.black)
+                .font(.system(size: 30))
+                .foregroundStyle(
+                    LinearGradient(
+                        colors: [.customGrayLight, .customGrayMedium],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+        }
+        .frame(width: 58, height: 58)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
